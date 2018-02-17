8534562d960f2c93aa2d815fb92bccf93200a864
Tien Nguyen <pri859@sudomail.com>
2018-02-17 01:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ var kitten = ["Milo", "Otis", "Garfield"];
 function removeFirstKitten() {
   return kitten.slice(1)
 kitten = kitten.slice(1)
-};
+}
 
 function removeLastKitten() {
 return kitten.slice(0, kitten.length-1)
 kitten = kitten.slice(0, kitten.length-1)
-};
+}
 
 function prependKitten(name) {
 return ["Arnold", ...kittens]
-};
+}
 
 function appendKitten(name) {
   return [...kittens, name]
-};
+}
+
+;
 
 function destructivelyAppendKitten() {
   return kitten.push("Ralph")
