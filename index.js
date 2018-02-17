b546e252624e9fefa4fcdb67ac4ca29eccadd707
Tien Nguyen <pri859@sudomail.com>
2018-02-17 00:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const app = "I don't do much."
+var kitten = ["Milo", "Otis", "Garfield"];
+
+function removeFirstKitten() {
+  return kitten.slice(1)
+  kitten = kitten.slice(1)
+}
+
+function removeLastKitten() {
+return kitten.slice(0, kitten.length-1)
+kitten = kitten.slice(0, kitten.length-1)
+}
+
+function prependKitten(name) {
+return ["Arnold", ...kittens]
+}
+
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function destructivelyAppendKitten(name) {
+  return [...kittens, "Ralph"]
+}
